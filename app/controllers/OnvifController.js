4f9de31b16b04a6a85c4edd4aa3a2b919a4d0fba
thalesgomes28 <thalesgomes28@gmail.com>
2020-09-02 12:35:48 -0300

    WIP

--- app/controllers/OnvifController.js
+++ app/controllers/OnvifController.js
@@ -1,8 +1,39 @@
-export default {
-  async showCamera(req, res, params) {
 
-    
-   
+
+const onvif = require("node-onvif");
+
+export default {
+  async showCamera({req, res, params}) {
+      console.log("==================\n IP camera solicitado:", params.id, "\n==================")
+    const addressCamera = params.id;
+    console.log("==================\n varriáveis de ambiente:", process.env.ADMIN, "\n==================")
+    let device = new onvif.OnvifDevice({
+      xaddr: addressCamera,
+      user: process.env.ADMIN,
+      pass: process.env.PASSWORD,
+    });
+
+    // Initialize 
+    device
+      .init()
+      .then(() => {
+        // Get the UDP stream URL
+        let url = device.getUdpStreamUrl();
+        console.log('URL:===============', url)
+        var resultado =
+          url.substr(0, 7)  + url.substr(7, 300);
+        console.log(resultado);
+        stream = new Stream({
+          name: "name",
+          streamUrl: resultado,
+          wsPort: 9000 + i,
+        });
+
+        //console.log("URL :"+url);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
     return res.json("A sua requisição será processada");
   },
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import "dotenv/config";
 
 import OnvifController from "./app/controllers/OnvifController";
-
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/show_camera/:id", OnvifController.showCamera);
-
-
 
 app.listen(3339, () => {
   console.log("Server running on localhost:3339");
